2693563e2ebc27089ea84fb21a2ec6087c42be5d
mjadach-iv <michal.jadach@hoprnet.org>
2025-07-17 00:28:56 +0200

    UI init

---
+++ ui/main/index.js
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron';
+
+const isDev = process.env.NODE_ENV === "development";
+let mainWindow = null;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 350,
+        height: 600,
+    });
+
+    if (isDev) {
+        // During development, load the React dev server
+        mainWindow.loadURL("http://localhost:3000"); // or your dev server port
+    } else {
+        // and load the index.html of the app.
+        mainWindow.loadFile(path.join(__dirname, "../renderer/dist/index.html"));
+    }
+
+}
+
+app.whenReady().then(createWindow);
