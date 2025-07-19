d0e4e25f82caece15c2bcce2ede2a18404c0f417
mjadach-iv <michal.jadach@hoprnet.org>
2025-07-19 16:20:25 +0200

    New start for the renderer

--- ui/main/index.js
+++ ui/main/index.js
@@ -11,7 +11,7 @@ function createWindow() {
 
     if (isDev) {
         // During development, load the React dev server
-        mainWindow.loadURL("http://localhost:3000"); // or your dev server port
+        mainWindow.loadURL("http://localhost:5173/"); // or your dev server port
     } else {
         // and load the index.html of the app.
         mainWindow.loadFile(path.join(__dirname, "../renderer/dist/index.html"));
